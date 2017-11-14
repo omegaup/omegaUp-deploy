91718fbb95dfe534bc663ecd9d19695ab3e8488c
Freddy Román <me@freddy.mx>
2017-11-14 11:11:26 -0600

    Initial support for libinteractive problems.

--- problem.py
+++ problem.py
@@ -6,22 +6,36 @@ from zipfile import *
 def enumerateFullPath(path):
         return [os.path.join(path, f) for f in os.listdir(path)]
 
+def enumerateFullPathWithNames(path):
+        return [(os.path.join(path, f), f) for f in os.listdir(path)]
+
 class Problem:
     def __init__(self, path):
         config = open(os.path.join(path, 'config.yaml'), 'r').read()
 
         self.path = path
         self.config = yaml.load(config)
+
         self.disabled = self.config.get('disabled', False)
         self.create = self.config.get('create', False)
+        self.interactive = self.config.get('interactive', False)
+        self.generateOutput = self.config.get('generate-output', False)
+
+        if self.generateOutput:
+            if self.interactive:
+                raise Exception("interactive and generate-output can't both be enabled")
+
+            self.solution = self.config['solution']
+            self.timeout = self.config['timeout']
+
         self.alias = self.config['alias']
 
     def prepareZip(self, zipPath):
         ins = [f for f in enumerateFullPath(os.path.join(self.path, 'cases')) if f.endswith('.in')]
         outs = []
 
-        if self.config['generate-output']:
-            subprocess.call(["g++", "-o", "solution", os.path.join(self.path, self.config['solution'])])
+        if self.generateOutput:
+            subprocess.call(["g++", "-o", "solution", os.path.join(self.path, self.solution)])
 
             for f_in in ins:
                 f_out = f_in[:-3] + '.out'
@@ -30,7 +44,7 @@ class Problem:
                     sol = subprocess.call('./solution',
                                           stdin = in_file,
                                           stdout = out_file,
-                                          timeout = self.config.get('timeout', 1))
+                                          timeout = self.timeout)
 
                 outs.append(f_out)
 
@@ -44,8 +58,13 @@ class Problem:
             raise Exception(missing_outs)
 
         with ZipFile(zipPath, 'w', compression = ZIP_DEFLATED) as archive:
-            archive.write(os.path.join(self.path, 'statements/es.markdown'), 'statements/es.markdown')
+            for (p, f) in enumerateFullPathWithNames(os.path.join(self.path, 'statements')):
+                archive.write(p, os.path.join('statements', f))
 
             for case in ins + outs:
                 _, name = os.path.split(case)
                 archive.write(case, os.path.join('cases', name))
+
+            if self.interactive:
+                for (p, f) in enumerateFullPathWithNames(os.path.join(self.path, 'interactive')):
+                    archive.write(p, os.path.join('interactive', f))
