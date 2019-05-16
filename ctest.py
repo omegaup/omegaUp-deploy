18b5097b18b759b9b25a0d6d3f81bb3216e82b2c
lhchavez <lhchavez@lhchavez.com>
2019-05-15 17:29:22 -0700

    Dejando de reinventar la rueda
    Por qué estamos reimplementando unittest.TestCase?

--- ctest.py
+++ ctest.py
@@ -1,18 +1,14 @@
 import sys
-import os
 import traceback
+import unittest
 
-from collections import deque
-
-class TestFailure(Exception):
-    pass
-
-class CTest:
-    def __init__(self):
+class CTest(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.input = sys.stdin.read()
         self.caseName = sys.argv[1]
 
-    def run(self):
+    def run(self, result=None):
         try:
             self.test()
         except:
@@ -20,41 +16,3 @@ class CTest:
             print(0)
         else:
             print(1)
-
-    def assertTrue(self, p):
-        if not p:
-            raise TestFailure()
-
-    def assertFalse(self, p):
-        if p:
-            raise TestFailure()
-
-    def assertEqual(self, p, q):
-        if p != q:
-            raise TestFailure(
-                'Equality failure!\nExpected: {}\nGot: {}'.format(q,p)
-            )
-
-    def assertNotEqual(self, p, q):
-        if p == q:
-            raise TestFailure(
-                'Inequality failure!\nGot: {}'.format(p)
-            )
-
-    def assertLess(self, p, q):
-        if p != q:
-            raise TestFailure(
-                'failure! {} is not ess than {}'.format(q,p)
-            )
-
-    def assertIn(self, p, q):
-        if p not in q:
-            raise TestFailure(
-                '{} not found in {}'.format(p,q)
-            )
-
-    def assertNotIn(self, p, q):
-        if p in q:
-            raise TestFailure(
-                '{} found in {}'.format(p,q)
-            )
