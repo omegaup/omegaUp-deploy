5a56bca36af8e0deadbe7e061cb1247d4f4c31ad
lhchavez <lhchavez@lhchavez.com>
2020-09-06 06:29:08 -0700

    Haciendo omegaUp mypy-compliant

--- omegaUp.py
+++ omegaUp.py
@@ -1,108 +1,135 @@
-import requests
 import logging
+import requests
 import sys
+import urllib.parse
+
+from typing import Any, BinaryIO, Dict, Mapping, Optional
+
+
+def _filterKey(x: Mapping[str, Any], k: str) -> Dict[str, Any]:
+    tmp: Dict[str, Any] = dict(x)
+    if k in tmp:
+        tmp[k] = 'REMOVED'
+    return tmp
+
+
+ApiReturnType = Dict[str, Any]
+
 
 class omegaUp:
-    url = "https://omegaup.com"
-    auth_token = None
+    def __init__(self, user: str, pwd: str):
+        self.user = user
+        self.pwd = pwd
+        self.auth_token: Optional[str] = None
+        self.url= 'https://omegaup.com'
 
-    def query(self, method, endpoint, payload={}, files=None, canFail=False):
-        def filterKey(x, k):
-            tmp = dict(x)
-            if k in tmp:
-                tmp[k] = 'REMOVED'
-            return tmp
+    def query(self,
+              method: str,
+              endpoint: str,
+              payload: Optional[Mapping[str, str]] = None,
+              files: Optional[Mapping[str, BinaryIO]] = None,
+              canFail: bool = False) -> ApiReturnType:
+        if payload is None:
+            payload = {}
+        else:
+            payload = dict(payload)
 
-        logging.info('Calling endpoint: ' + endpoint)
-        logging.info('Payload: ' + str(filterKey(payload, 'password')))
+        logging.info('Calling endpoint: %s', endpoint)
+        logging.info('Payload: %s', _filterKey(payload, 'password'))
 
         if self.auth_token is not None:
             payload['ouat'] = self.auth_token
 
-        longTime = 60 * 10 # 10 minutes
+        longTime = 60 * 10  # 10 minutes
 
         if method == 'GET':
-            r = requests.get(self.url + endpoint, params=payload, timeout=longTime)
+            r = requests.get(urllib.parse.urljoin(self.url, endpoint),
+                             params=payload,
+                             timeout=longTime)
         elif method == 'POST':
-            r = requests.post(self.url + endpoint, params=payload, files=files, timeout=longTime)
+            r = requests.post(urllib.parse.urljoin(self.url, endpoint),
+                              params=payload,
+                              files=files,
+                              timeout=longTime)
         else:
-            raise Exception(method)
+            raise NotImplementedError(method)
 
         try:
-            response = r.json()
-        except Exception:
+            response: ApiReturnType = r.json()
+        except:
             logging.exception(r.text)
             raise
 
-        logging.info('Response: ' + str(filterKey(response, 'auth_token')))
+        logging.info('Response: %s', _filterKey(response, 'auth_token'))
 
         if not canFail and response['status'] != 'ok':
             raise Exception(response)
 
         return response
 
-    def login(self):
+    def login(self) -> None:
         payload = {"usernameOrEmail": self.user, "password": self.pwd}
 
         response = self.query("GET", "/api/user/login", payload)
 
         self.auth_token = response['auth_token']
 
-    def session(self):
+    def session(self) -> ApiReturnType:
         return self.query("GET", "/api/session/currentsession")
 
-    def isProblemAdmin(self, alias):
+    def isProblemAdmin(self, alias: str) -> bool:
         return True  # welp
         # payload = { 'problem_alias': alias }
         # response = self.query("GET", "/api/problem/stats", payload, canFail = True)
         # return response['status'] == 'ok'
 
-    def problemExists(self, alias):
+    def problemExists(self, alias: str) -> bool:
         payload = {'problem_alias': alias}
-        response = self.query(
-            "GET", "/api/problem/details", payload, canFail=True)
+        response = self.query("GET",
+                              "/api/problem/details",
+                              payload,
+                              canFail=True)
         return response.get('status') == 'ok'
 
-    def problemTags(self, alias):
+    def problemTags(self, alias: str) -> ApiReturnType:
         payload = {'problem_alias': alias}
         return self.query("GET", "/api/problem/tags", payload)
 
-    def addProblemTag(self, alias, tag, visibility):
-        payload = {'problem_alias': alias,
-                   'name': tag,
-                   'public': visibility}
+    def addProblemTag(self, alias: str, tag: str,
+                      visibility: bool) -> ApiReturnType:
+        payload = {
+            'problem_alias': alias,
+            'name': tag,
+            'public': 'true' if visibility else 'false',
+        }
         return self.query("GET", "/api/problem/addTag", payload)
 
-    def removeProblemTag(self, alias, tag):
-        payload = {'problem_alias': alias,
-                   'name': tag}
+    def removeProblemTag(self, alias: str, tag: str) -> ApiReturnType:
+        payload = {'problem_alias': alias, 'name': tag}
         return self.query("GET", "/api/problem/removeTag", payload)
 
-    def problemAdmins(self, alias):
+    def problemAdmins(self, alias: str) -> ApiReturnType:
         payload = {'problem_alias': alias}
         return self.query("GET", "/api/problem/admins", payload)
 
-    def addAdmin(self, alias, user):
-        payload = {'problem_alias': alias,
-                   "usernameOrEmail": user}
+    def addAdmin(self, alias: str, user: str) -> ApiReturnType:
+        payload = {'problem_alias': alias, "usernameOrEmail": user}
         return self.query("GET", "/api/problem/addAdmin", payload)
 
-    def removeAdmin(self, alias, user):
-        payload = {'problem_alias': alias,
-                   "usernameOrEmail": user}
+    def removeAdmin(self, alias: str, user: str) -> ApiReturnType:
+        payload = {'problem_alias': alias, "usernameOrEmail": user}
         return self.query("GET", "/api/problem/removeAdmin", payload)
 
-    def addAdminGroup(self, alias, group):
-        payload = {'problem_alias': alias,
-                   "group": group}
+    def addAdminGroup(self, alias: str, group: str) -> ApiReturnType:
+        payload = {'problem_alias': alias, "group": group}
         return self.query("GET", "/api/problem/addGroupAdmin", payload)
 
-    def removeAdminGroup(self, alias, group):
-        payload = {'problem_alias': alias,
-                   "group": group}
+    def removeAdminGroup(self, alias: str, group: str) -> ApiReturnType:
+        payload = {'problem_alias': alias, "group": group}
         return self.query("GET", "/api/problem/removeGroupAdmin", payload)
 
-    def uploadProblem(self, problem, canCreate, zipPath, message):
+    def uploadProblem(self, problem: Mapping[str, Any], canCreate: bool,
+                      zipPath: str, message: str) -> None:
         misc = problem['misc']
         alias = misc['alias']
         limits = problem['limits']
@@ -142,11 +169,25 @@ class omegaUp:
         else:
             endpoint = "/api/problem/update"
 
-
         languages = payload.get('languages', '')
 
         if languages == 'all':
-            payload['languages'] = 'c11-gcc,c11-clang,cpp11-gcc,cpp11-clang,cpp17-gcc,cpp17-clang,cs,hs,java,lua,pas,py2,py3,rb'
+            payload['languages'] = ''.join((
+                'c11-gcc',
+                'c11-clang',
+                'cpp11-gcc',
+                'cpp11-clang',
+                'cpp17-gcc',
+                'cpp17-clang',
+                'cs',
+                'hs',
+                'java',
+                'lua',
+                'pas',
+                'py2',
+                'py3',
+                'rb',
+            ))
         elif languages == 'karel':
             payload['languages'] = 'kj,kp'
         elif languages == 'none':
@@ -163,9 +204,10 @@ class omegaUp:
             allAdmins = self.problemAdmins(alias)
 
         if targetAdmins is not None:
-            admins = {a['username'].lower()
-                      for a in allAdmins['admins']
-                      if a['role'] == 'admin'}
+            admins = {
+                a['username'].lower()
+                for a in allAdmins['admins'] if a['role'] == 'admin'
+            }
 
             desiredAdmins = {admin.lower() for admin in targetAdmins}
 
@@ -181,9 +223,10 @@ class omegaUp:
                 self.removeAdmin(alias, admin)
 
         if targetAdminGroups is not None:
-            adminGroups = {a['alias'].lower()
-                           for a in allAdmins['group_admins']
-                           if a['role'] == 'admin'}
+            adminGroups = {
+                a['alias'].lower()
+                for a in allAdmins['group_admins'] if a['role'] == 'admin'
+            }
 
             desiredGroups = {group.lower() for group in targetAdminGroups}
 
@@ -199,8 +242,7 @@ class omegaUp:
                 self.removeAdminGroup(alias, group)
 
         if 'tags' in misc:
-            tags = {t['name'].lower() for t in
-                    self.problemTags(alias)['tags']}
+            tags = {t['name'].lower() for t in self.problemTags(alias)['tags']}
 
             desiredTags = {t.lower() for t in misc['tags']}
 
@@ -215,9 +257,4 @@ class omegaUp:
 
             for tag in tagsToAdd:
                 logging.info('Adding problem tag: ' + tag)
-                self.addProblemTag(alias, tag,
-                                   payload.get('visibility', '0'))
-
-    def __init__(self, user, pwd):
-        self.user = user
-        self.pwd = pwd
+                self.addProblemTag(alias, tag, payload.get('visibility', '0'))
