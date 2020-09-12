6db6d30b4228ec87365b6d5526cef5dba133ae5b
lhchavez <lhchavez@lhchavez.com>
2020-09-11 17:27:08 -0700

    Refactor generateouts.py
    In the CI, the .out files are already generated by runtests.py. So now
    this needs to be able to just generate the testplan and .png files.

--- generateouts.py
+++ generateouts.py
@@ -9,68 +9,89 @@ import re
 import subprocess
 import sys
 
-from typing import List
+from typing import List, Optional
 
 import container
 import problems
 
+_SUPPORTED_GENERATORS = frozenset(('out', 'png', 'testplan'))
 
-def _generateOutputs(p: problems.Problem, *, rootDirectory: str, force: bool,
-                     ci: bool) -> bool:
-    """Generate .out and .png files for the provided problem."""
-    logging.info('%-30s: Generating outputs for problem', p.title)
 
-    pPath = os.path.join(rootDirectory, p.path)
+def _getSolution(p: problems.Problem, *, rootDirectory: str,
+                 ci: bool) -> Optional[str]:
+    """Gets the solution for the problem."""
+    solutions = [
+        f for f in os.listdir(os.path.join(rootDirectory, p.path, 'solutions'))
+        if f.startswith('solution.')
+    ]
 
-    if 'cases' in p.config:
-        testplan = os.path.join(pPath, 'testplan')
-
-        logging.info('%-30s: Generating testplan from settings.json.', p.title)
-
-        if os.path.isfile(testplan):
-            problems.fatal(
-                'testplan cannot exist when settings.json has cases!',
-                filename=os.path.relpath(testplan, rootDirectory),
-                ci=ci)
-
-        with open(testplan, 'w') as tp:
-            for group in p.config['cases']:
-                for case in group['cases']:
-                    tp.write("{} {}\n".format(case['name'], case['weight']))
-
-    languages = p.config['misc']['languages']
-    if languages == 'none':
-        return True
-
-    generators = [x for x in os.listdir(pPath) if x.startswith('generator')]
-
-    if not generators:
-        logging.warning('%-30s: No generator found! Skipping.', p.title)
-        # TODO: check .ins and .outs match
-        return True
-    if len(generators) != 1:
-        problems.fatal(f'Found more than one generator! {generators}',
+    if not solutions:
+        return None
+    if len(solutions) != 1:
+        problems.fatal(f'Found more than one solution! {solutions}',
                        filename=os.path.join(p.path, 'settings.json'),
                        ci=ci)
 
-    genPath = os.path.join(pPath, generators[0])
-    relativeGenPath = os.path.relpath(genPath, rootDirectory)
+    return os.path.join(rootDirectory, p.path, 'solutions', solutions[0])
 
-    anyProblemFailure = False
+
+def _getInputs(p: problems.Problem, *, rootDirectory: str,
+               ci: bool) -> List[str]:
+    """Gets the list of .in files for the problem."""
+    inFilenames = [
+        f for subdirectory in ('cases', 'examples', 'statements')
+        for f in problems.enumerateFullPath(
+            os.path.join(rootDirectory, p.path, subdirectory))
+        if f.endswith('.in')
+    ]
+    if not inFilenames:
+        problems.fatal(f'No test cases found for {p.title}!',
+                       filename=os.path.join(p.path, 'settings.json'),
+                       ci=ci)
+    return inFilenames
+
+
+def _generateTestplan(p: problems.Problem, *, rootDirectory: str, force: bool,
+                      ci: bool) -> bool:
+    """Generate testplan files for the provided problem."""
+    logging.info('%-30s: Generating testplan for problem', p.title)
+
+    if 'cases' not in p.config:
+        return True
+
+    testplan = os.path.join(rootDirectory, p.path, 'testplan')
+
+    logging.info('%-30s: Generating testplan from settings.json.', p.title)
+
+    if os.path.isfile(testplan):
+        problems.fatal('testplan cannot exist when settings.json has cases!',
+                       filename=os.path.relpath(testplan, rootDirectory),
+                       ci=ci)
+
+    with open(testplan, 'w') as tp:
+        for group in p.config['cases']:
+            for case in group['cases']:
+                tp.write("{} {}\n".format(case['name'], case['weight']))
+
+    return True
+
+
+def _generateOutputs(p: problems.Problem, *, rootDirectory: str, force: bool,
+                     ci: bool) -> bool:
+    """Generate .out files for the provided problem."""
+    logging.info('%-30s: Generating outputs for problem', p.title)
+
+    solutionPath = _getSolution(p, rootDirectory=rootDirectory, ci=ci)
+    if solutionPath is None:
+        logging.warning('%-30s: No solution found! Skipping.', p.title)
+        return True
+    relativeSolutionPath = os.path.relpath(solutionPath, rootDirectory)
+
+    inFilenames = _getInputs(p, rootDirectory=rootDirectory, ci=ci)
 
     # TODO: if karel, enforce examples
-    with container.Compile(sourcePath=genPath, ci=ci) as c:
-        inFilenames = [
-            f for subdirectory in ('cases', 'examples', 'statements')
-            for f in problems.enumerateFullPath(
-                os.path.join(pPath, subdirectory)) if f.endswith('.in')
-        ]
-
-        if not inFilenames:
-            problems.fatal(f'No test cases found for {p.title}!',
-                           filename=os.path.join(p.path, 'settings.json'),
-                           ci=ci)
-
+    anyProblemFailure = False
+    with container.Compile(sourcePath=solutionPath, ci=ci) as c:
         for inFilename in inFilenames:
             relativeInFilename = os.path.relpath(inFilename, rootDirectory)
             outFilename = f'{os.path.splitext(inFilename)[0]}.out'
@@ -81,7 +102,7 @@ def _generateOutputs(p: problems.Problem, *, rootDirectory: str, force: bool,
 
             if not force and os.path.isfile(outFilename):
                 problems.fatal(
-                    (f".outs can't be present when generator.$lang$ "
+                    (f".outs can't be present when solutions/solution.$lang$ "
                      f"exists: {relativeOutFilename}"),
                     filename=relativeOutFilename,
                     ci=ci)
@@ -93,72 +114,99 @@ def _generateOutputs(p: problems.Problem, *, rootDirectory: str, force: bool,
             except subprocess.CalledProcessError as cpe:
                 anyProblemFailure = True
                 with open(outFilename, 'r') as f:
-                    problems.error((f'{relativeGenPath} failed running '
+                    problems.error((f'{relativeSolutionPath} failed running '
                                     f'with {relativeInFilename}:\n'
                                     f'stdout:\n{f.read()}\n'
                                     f'stderr:\n{cpe.stderr.decode("utf-8")}'),
-                                   filename=relativeGenPath,
+                                   filename=relativeSolutionPath,
                                    ci=ci)
-
-        if languages == 'karel':
-            logging.info('%-30s: Generating pngs for problem', p.title)
-
-            for inFilename in inFilenames:
-                logging.debug('%-30s: Generating .pngs for %s', p.title,
-                              inFilename)
-                dimMatch = re.search(r'\.(\d*)x(\d*)\.in', inFilename)
-                if dimMatch:
-                    dimOpts = [
-                        '--height',
-                        dimMatch.group(1), '--width',
-                        dimMatch.group(2)
-                    ]
-                else:
-                    dimOpts = []
-
-                try:
-                    c.run_command([
-                        '/opt/nodejs/lib/node_modules/karel/cmd/kareljs',
-                        'draw',
-                        '--output=-',
-                    ] + dimOpts,
-                                  stdinPath=inFilename,
-                                  stdoutPath=f'{inFilename}.png',
-                                  timeout=datetime.timedelta(seconds=10))
-                except subprocess.CalledProcessError as cpe:
-                    anyProblemFailure = True
-                    problems.error((f'failed generating '
-                                    f'input .png for {relativeInFilename}:\n' +
-                                    cpe.stderr.decode("utf-8")),
-                                   filename=relativeInFilename,
-                                   ci=ci)
-                    continue
-
-                try:
-                    c.run_command([
-                        '/opt/nodejs/lib/node_modules/karel/cmd/kareljs',
-                        'draw',
-                        '--output=-',
-                        '--run',
-                        os.path.join('/src', c.containerSourceFilename),
-                    ] + dimOpts,
-                                  stdinPath=inFilename,
-                                  stdoutPath=f'{outFilename}.png',
-                                  timeout=datetime.timedelta(seconds=10))
-                except subprocess.CalledProcessError as cpe:
-                    anyProblemFailure = True
-                    problems.error(
-                        (f'{relativeGenPath} failed generating '
-                         f'output .png with {relativeInFilename}:\n' +
-                         cpe.stderr.decode("utf-8")),
-                        filename=relativeGenPath,
-                        ci=ci)
 
     if anyProblemFailure:
         logging.warning('%-30s: Failed generating some outputs', p.title)
         return False
 
     logging.info('%-30s: Success generating outputs', p.title)
+    return True
+
+
+def _generateImages(p: problems.Problem, *, rootDirectory: str, force: bool,
+                    ci: bool) -> bool:
+    """Generate .png files for the provided problem."""
+    logging.info('%-30s: Generating outputs for problem', p.title)
+
+    pPath = os.path.join(rootDirectory, p.path)
+
+    solutionPath = _getSolution(p, rootDirectory=rootDirectory, ci=ci)
+    if solutionPath is None:
+        logging.warning('%-30s: No solution found! Skipping.', p.title)
+        return True
+    relativeSolutionPath = os.path.relpath(solutionPath, rootDirectory)
+
+    inFilenames = _getInputs(p, rootDirectory=rootDirectory, ci=ci)
+
+    anyProblemFailure = False
+    with container.Compile(sourcePath=solutionPath, ci=ci) as c:
+        logging.info('%-30s: Generating pngs for problem', p.title)
+
+        for inFilename in inFilenames:
+            relativeInFilename = os.path.relpath(inFilename, rootDirectory)
+            outFilename = f'{os.path.splitext(inFilename)[0]}.out'
+            relativeOutFilename = os.path.relpath(outFilename, rootDirectory)
+
+            logging.debug('%-30s: Generating .pngs for %s', p.title,
+                          inFilename)
+            dimMatch = re.search(r'\.(\d*)x(\d*)\.in', inFilename)
+            if dimMatch:
+                dimOpts = [
+                    '--height',
+                    dimMatch.group(1), '--width',
+                    dimMatch.group(2)
+                ]
+            else:
+                dimOpts = []
+
+            try:
+                c.run_command([
+                    '/opt/nodejs/lib/node_modules/karel/cmd/kareljs',
+                    'draw',
+                    '--output=-',
+                ] + dimOpts,
+                              stdinPath=inFilename,
+                              stdoutPath=f'{inFilename}.png',
+                              timeout=datetime.timedelta(seconds=10))
+            except subprocess.CalledProcessError as cpe:
+                anyProblemFailure = True
+                problems.error((f'failed generating '
+                                f'input .png for {relativeInFilename}:\n' +
+                                cpe.stderr.decode("utf-8")),
+                               filename=relativeInFilename,
+                               ci=ci)
+                continue
+
+            try:
+                c.run_command([
+                    '/opt/nodejs/lib/node_modules/karel/cmd/kareljs',
+                    'draw',
+                    '--output=-',
+                    '--run',
+                    os.path.join('/src', c.containerSourceFilename),
+                ] + dimOpts,
+                              stdinPath=inFilename,
+                              stdoutPath=f'{outFilename}.png',
+                              timeout=datetime.timedelta(seconds=10))
+            except subprocess.CalledProcessError as cpe:
+                anyProblemFailure = True
+                problems.error((f'{relativeSolutionPath} failed generating '
+                                f'output .png with {relativeInFilename}:\n' +
+                                cpe.stderr.decode("utf-8")),
+                               filename=relativeSolutionPath,
+                               ci=ci)
+
+    if anyProblemFailure:
+        logging.warning('%-30s: Failed generating some outputs', p.title)
+        return False
+
+    logging.info('%-30s: Success generating .png files', p.title)
     return True
 
 
@@ -178,6 +226,12 @@ def _main() -> None:
                         '-j',
                         default=min(32, (os.cpu_count() or 2) + 4),
                         help='Number of threads to run concurrently')
+    parser.add_argument('--generate',
+                        default=_SUPPORTED_GENERATORS,
+                        type=lambda x: set(x.split(',')),
+                        help=('Comma-separated list of artifacts to generate. '
+                              'Should be a subset of {out,png,testplan}. '
+                              'Generates everything by default.'))
     parser.add_argument('--verbose',
                         action='store_true',
                         help='Verbose logging')
@@ -186,6 +240,11 @@ def _main() -> None:
                         type=str,
                         nargs='*')
     args = parser.parse_args()
+
+    if args.generate - _SUPPORTED_GENERATORS:
+        logging.error('Provided generators not supported: %r',
+                      args.generate - _SUPPORTED_GENERATORS)
+        sys.exit(1)
 
     logging.basicConfig(format='%(asctime)s: %(message)s',
                         level=logging.DEBUG if args.verbose else logging.INFO)
@@ -200,12 +259,27 @@ def _main() -> None:
         for p in problems.problems(allProblems=args.all,
                                    rootDirectory=rootDirectory,
                                    problemPaths=args.problem_paths):
-            futures.append(
-                executor.submit(_generateOutputs,
-                                p,
-                                rootDirectory=rootDirectory,
-                                force=args.force,
-                                ci=args.ci))
+            if 'testplan' in args.generate:
+                futures.append(
+                    executor.submit(_generateTestplan,
+                                    p,
+                                    rootDirectory=rootDirectory,
+                                    force=args.force,
+                                    ci=args.ci))
+            if 'out' in args.generate:
+                futures.append(
+                    executor.submit(_generateOutputs,
+                                    p,
+                                    rootDirectory=rootDirectory,
+                                    force=args.force,
+                                    ci=args.ci))
+            if 'png' in args.generate:
+                futures.append(
+                    executor.submit(_generateImages,
+                                    p,
+                                    rootDirectory=rootDirectory,
+                                    force=args.force,
+                                    ci=args.ci))
 
         if not all(future.result()
                    for future in concurrent.futures.as_completed(futures)):
