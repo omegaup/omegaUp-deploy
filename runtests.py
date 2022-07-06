95d2d52c75bfbce5955e18264511cc7d647aca3f
Freddy Román <me@freddy.mx>
2022-07-05 21:46:43 -0700

    bump

--- runtests.py
+++ runtests.py
@@ -377,7 +377,8 @@ def _main() -> None:
                     filename=path,
                     ci=args.ci)
 
-        problems.warning(f'Missing invalid test cases for problem: {p.title}')
+        problems.warning(f'Missing invalid test cases for problem: {p.title}',
+                         ci=args.ci)
 
         logging.info(f'Results for {p.title}: {report["state"]}')
         logging.info(f'    Full logs and report in {problemResultsDirectory}')
